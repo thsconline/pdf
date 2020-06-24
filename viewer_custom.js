009e603a471e67b72cf66e230e1a392e30906fe2
thsconline <thsconline@users.noreply.github.com>
2020-06-25 01:04:12 +1000

    Update viewer_custom.js

--- viewer_custom.js
+++ viewer_custom.js
@@ -1,3 +1,87 @@
+var mybrowser;
+var myversion;
+// this code from https://jsfiddle.net/kmturley/Gd6c8/
+(function () {
+    'use strict';
+    
+    var module = {
+        options: [],
+        header: [navigator.platform, navigator.userAgent, navigator.appVersion, navigator.vendor, window.opera],
+        dataos: [
+            { name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+            { name: 'Windows', value: 'Win', version: 'NT' },
+            { name: 'iPhone', value: 'iPhone', version: 'OS' },
+            { name: 'iPad', value: 'iPad', version: 'OS' },
+            { name: 'Kindle', value: 'Silk', version: 'Silk' },
+            { name: 'Android', value: 'Android', version: 'Android' },
+            { name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+            { name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+            { name: 'Macintosh', value: 'Mac', version: 'OS X' },
+            { name: 'Linux', value: 'Linux', version: 'rv' },
+            { name: 'Palm', value: 'Palm', version: 'PalmOS' }
+        ],
+        databrowser: [
+            { name: 'Chrome', value: 'Chrome', version: 'Chrome' },
+            { name: 'Firefox', value: 'Firefox', version: 'Firefox' },
+            { name: 'Safari', value: 'Safari', version: 'Version' },
+            { name: 'Internet Explorer', value: 'MSIE', version: 'MSIE' },
+            { name: 'Opera', value: 'Opera', version: 'Opera' },
+            { name: 'BlackBerry', value: 'CLDC', version: 'CLDC' },
+            { name: 'Mozilla', value: 'Mozilla', version: 'Mozilla' }
+        ],
+        init: function () {
+            var agent = this.header.join(' '),
+                os = this.matchItem(agent, this.dataos),
+                browser = this.matchItem(agent, this.databrowser);
+            
+            return { os: os, browser: browser };
+        },
+        matchItem: function (string, data) {
+            var i = 0,
+                j = 0,
+                html = '',
+                regex,
+                regexv,
+                match,
+                matches,
+                version;
+            
+            for (i = 0; i < data.length; i += 1) {
+                regex = new RegExp(data[i].value, 'i');
+                match = regex.test(string);
+                if (match) {
+                    regexv = new RegExp(data[i].version + '[- /:;]([\\d._]+)', 'i');
+                    matches = string.match(regexv);
+                    version = '';
+                    if (matches) { if (matches[1]) { matches = matches[1]; } }
+                    if (matches) {
+                        matches = matches.split(/[._]+/);
+                        for (j = 0; j < matches.length; j += 1) {
+                            if (j === 0) {
+                                version += matches[j] + '.';
+                            } else {
+                                version += matches[j];
+                            }
+                        }
+                    } else {
+                        version = '0';
+                    }
+                    return {
+                        name: data[i].name,
+                        version: parseFloat(version)
+                    };
+                }
+            }
+            return { name: 'unknown', version: 0 };
+        }
+    };
+    
+    var e = module.init();
+	mybrowser = e.browser.name;
+    	myversion = e.browser.version;
+	
+}());
+
 /**
  * @licstart The following is the entire license notice for the
  * Javascript code in this page
@@ -14270,22 +14354,52 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 var DISABLE_CREATE_OBJECT_URL = _pdfjsLib.apiCompatibilityParams.disableCreateObjectURL || false;
 
 function _download(blobUrl, filename) {
-  var a = document.createElement('a');
 
+//CHROME83 fix
+if(browser == "Chrome" && version > 82)
+{
+  var a = document.createElement('a');	
   if (!a.click) {
-    throw new Error('DownloadManager: "a.click()" is not supported.');
-  }
-
-  a.href = blobUrl;
-  a.target = '_parent';
-
-  if ('download' in a) {
-    a.download = filename;
-  }
-
+     throw new Error('DownloadManager: "a.click()" is not supported.');
+   }
+  a.href= altDownloadUrl;
+  a.target = '_blank';
   (document.body || document.documentElement).appendChild(a);
   a.click();
-  a.remove();
+  a.remove();	
+}
+else
+{
+ try
+ {
+   var a = document.createElement('a');
+ 
+   if (!a.click) {
+     throw new Error('DownloadManager: "a.click()" is not supported.');
+   }
+
+   a.href = blobUrl;
+   a.target = '_parent';
+
+   if ('download' in a) {
+     a.download = filename;
+   }
+ 
+   (document.body || document.documentElement).appendChild(a);
+   a.click();
+   a.remove();
+ }
+ catch(err)
+ {
+   var a = document.createElement('a');	
+   a.href= altDownloadUrl;
+   a.target = '_blank';
+   (document.body || document.documentElement).appendChild(a);
+   a.click();
+   a.remove();	
+ }
+}
+
 }
 
 var DownloadManager =
@@ -15528,4 +15642,3 @@ catch(err)
 	
 	
 }
-
