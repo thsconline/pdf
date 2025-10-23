97d242bba7a58c4bf19dd9e64fb48d26d031b454
thsconline <thsconline@gmail.com>
2025-10-23 21:56:07 +1000

    Update viewer_custom2.js

--- viewer_custom2.js
+++ viewer_custom2.js
@@ -15249,7 +15249,17 @@ function dispatchEvent(eventType) {
   window.dispatchEvent(event);
 }
 
-
+function PDFPrintService(pdfDocument, pagesOverview, printContainer, l10n) {
+  this.pdfDocument = pdfDocument;
+  this.pagesOverview = pagesOverview;
+  this.printContainer = printContainer;
+  this.l10n = l10n || _ui_utils.NullL10n;
+  this.disableCreateObjectURL = pdfDocument.loadingParams['disableCreateObjectURL'];
+  this.currentPage = -1;
+  this.scratchCanvas = document.createElement('canvas');
+}
+PDFPrintService.prototype = {
+};
 
 /***/ })
 /******/ ]);
